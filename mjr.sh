ca341fbb540e6fa91bc319a953d25860c54b45a4
MasterJ <masterj122517@163.com>
2023-01-21 13:49:42 +0800

    packerinstlal

--- mjr.sh
+++ mjr.sh
@@ -264,6 +264,7 @@ sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
 sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 
 manualinstall yay || error "Failed to install AUR helper."
+packerinstall
 
 # The command that does all the installing. Reads the progs.csv file and
 # installs each needed program the way required. Be sure to run this only after

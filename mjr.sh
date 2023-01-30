c749d75b0b2819d9603a5a5c32f026e90e0a86a5
MasterJ <masterj122517@163.com>
2023-01-30 17:00:27 +0800

    把仓库变为acwing

--- mjr.sh
+++ mjr.sh
@@ -2,7 +2,7 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/masterj122517/rice.git"
+dotfilesrepo="https://git.acwing.com/MasterJ/rice.git"
 progsfile="https://raw.githubusercontent.com/masterj122517/MJR/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
